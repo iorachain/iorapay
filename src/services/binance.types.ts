cb017bc786fd56b001b04c89f2a434a58fc17659
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-02-09 07:50:25 -0300

    erase binance stuffs

--- src/services/binance.types.ts
+++
@@ -1,3 +0,0 @@
-export default interface PRICES_CURRENCIES {
-  symbol?: string | null;
-}
--- src/utils/urls.ts
+++
@@ -1,9 +0,0 @@
-export const URLS = {
-  BINANCE_ENDPOINT: "https://api.binance.com",
-  BINANCE_APIKEY:
-    "uB4Z2uah4bwWVr8eykwcEjch00DVHquankHLNprMsN09JecEYokZp1dURjhTlxH3",
-  BINANCE_SECRET_KEY:
-    "52Lj0FquDuSyQT54Kv6p9KtucVXg2ttNH9qs0CCkhWtCiAjIDIAHw9nPJslAwH9j",
-};
-
-export default URLS;
