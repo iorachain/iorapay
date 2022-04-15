ceaf317b08be60750a76c666446163fc7ea030ad
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-04-15 16:43:51 -0300

    auth and redux installed

---
+++ src/redux/reducers/uiReducers.ts
@@ -0,0 +1,30 @@
+import { SET_ERRORS, LOADING_UI, CLEAR_ERRORS } from "../types.d";
+
+const initialState = {
+  loading: false,
+  errors: "",
+};
+
+export default function (state = initialState, action: any) {
+  switch (action.type) {
+    case SET_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: null,
+      };
+    case LOADING_UI:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+}
---
+++ src/redux/store.ts
@@ -0,0 +1,31 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import userReducer from "./reducers/userReducers";
+import uiReducer from "./reducers/uiReducers";
+
+const initialState = {};
+const middleware = [thunk];
+
+// Redux Devtool
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
+  }
+}
+
+// All Reducers
+const reducer = combineReducers({
+  user: userReducer,
+  UI: uiReducer,
+});
+
+const store = createStore(
+  reducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()) as any
+  )
+);
+export default store;
